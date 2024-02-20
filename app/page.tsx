e61fa241b1e3fad5102e0289f859294d705582b5
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-02-20 17:02:29 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Button className="px-10">
+      <Button  className="px-10">
         Hello
       </Button>
     </main>
