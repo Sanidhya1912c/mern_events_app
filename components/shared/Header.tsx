82b75c9bfa9292e63c1d47dd4e920d52684892ab
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-02-26 17:00:37 +0530

    .

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Button } from "../ui/button";
-import NavItems from "./NavItems";
-import MobileNav from "./MobileNav";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper  flex intems-center justify-between">
-        <Link href="/" className="w-36 ">
-          <Image
-            src="/assets/images/logo.svg"
-            width={128}
-            height={38}
-            alt="EventlyLogo"
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
           />
         </Link>
 
@@ -31,14 +28,16 @@ const Header = () => {
             <MobileNav />
           </SignedIn>
           <SignedOut>
-            <Button asChild className="rounded-full size-lg">
-              <Link href="/sign-in"></Link>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
             </Button>
           </SignedOut>
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
